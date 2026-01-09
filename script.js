053980cf54d44150dd6be4769ee2377080d0d632
juseven70 <ju.seventytwenty@gmail.com>
2026-01-09 09:36:19 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 const display = document.getElementById("display");
+const history = document.getElementById("history");
 
 function appendValue(value) {
   display.value += value;
 }
 
-function clearDisplay() {
+function operator(op) {
+  if (display.value === "") return;
+
+  history.textContent += display.value + op + "\n";
   display.value = "";
 }
 
-function calculate() {
-  display.value = eval(display.value);
-}
 function deleteOne() {
   display.value = display.value.slice(0, -1);
+}
+
+function clearAll() {
+  display.value = "";
+  history.textContent = "";
+}
+
+function calculate() {
+  let expression = history.textContent.replace(/\n/g, "") + display.value;
+
+  display.value = eval(expression);
+  history.textContent = "";
 }
